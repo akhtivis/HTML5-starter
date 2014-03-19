2f9ad15ded967d18ecfb7206ec3f5bac3ebeea01
James <james@seamlessthemes.com>
2014-03-19 09:51:11 -0400

    Initial comment. Everything is new!

---
+++ config.rb
@@ -0,0 +1,16 @@
+# compass config.rb refrences - http://compass-style.org/help/tutorials/configuration-reference/
+
+# path to file structures
+css_dir = "." # css generate to project root
+sass_dir = "." # sass folder is in project root too
+images_dir = "images" # please do not remove this line
+javascripts_dir = "js"
+    
+# output_style options - :nested, :expanded, :compact, :compressed
+output_style = :expanded
+
+# line_comments options - true, false
+line_comments = false
+sass_options = {:debug_info=>false} # by Compass.app 
+line_comments = false # by Compass.app 
+output_style = :expanded # by Compass.app 
